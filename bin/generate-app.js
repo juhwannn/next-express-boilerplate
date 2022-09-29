adee5d986cc6df623a5378726fdb66ef8cb3b65b
ahnjh <algorithm123@naver.com>
2022-09-29 13:25:31 +0900

    update: generate-app

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 if (process.argv.length < 3) {
     console.log('You have to provide a name to your app.');
     console.log('For example :');
-    console.log('    npx create-my-boilerplate my-app');
+    console.log('npx create-next-express {FOLDER_NAME}');
     process.exit(1);
 }
 
@@ -17,34 +17,49 @@ const projectPath = path.join(currentPath, projectName);
 const git_repo = "https://github.com/juhwannn/next-express-boilerplate";
 
 try {
-  fs.mkdirSync(projectPath);
+    fs.mkdirSync(projectPath);
 } catch (err) {
-  if (err.code === 'EEXIST') {
-    console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
-  } else {
-    console.log(error);
-  }
-  process.exit(1);
+    if (err.code === 'EEXIST') {
+        console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+    } else {
+        console.log(error);
+    }
+    process.exit(1);
 }
 
 async function main() {
     try {
-      console.log('Downloading files...');
-      execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+        console.log("@@@");
 
-      process.chdir(projectPath);
+        console.log('Downloading files...');
+        execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
-      console.log('Installing dependencies...');
-      execSync('npm install');
+        process.chdir(projectPath);
 
-      console.log('Removing useless files');
-      execSync('npx rimraf ./.git');
-      fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
+        console.log("Installing dependencies...");
+        execSync('npm install');
 
-      console.log('The installation is done, this is ready to use !');
+        console.log("Removing useless files");
+        execSync('rm -rf ./.git');
+        fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
+
+        console.log("Changing the package.json file");
+        fs.readFile("./package.json", "utf-8", (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            console.log("package.json data : ");
+            console.log(data);
+        });
+
+
+        console.log("The installation is done, this is ready to use !");
 
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
 }
+
+
 main();
