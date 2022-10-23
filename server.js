9e528d31bf876bb386bcd1a8ab010b4b72370717
ahnjh <algorithm123@naver.com>
2022-10-23 19:48:13 +0900

    add: swagger

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const express = require('express');
 const Next = require('next');
 const nextJs = Next({dev});
 const nextJsRequestHandler = nextJs.getRequestHandler();
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 (async () => {
     try {
@@ -20,6 +21,24 @@ const nextJsRequestHandler = nextJs.getRequestHandler();
 
         expressServer.set('trust proxy', true);
         expressServer.use('/api', require('./serverSides/routes/api'));
+
+        const options = {
+            definition: {
+                openapi: '3.0.0',
+                info: {
+                    title: `next-express-boilerplate`,
+                    version: '1.0.0',
+                },
+            },
+            apis: ['./serverSides/routes/api/*.js'],
+        };
+
+        const swaggerUiOptions = {
+            explorer: true,
+        };
+
+        const openapiSpecification = swaggerJsdoc(options);
+        expressServer.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification, swaggerUiOptions));
 
         if (nextJsEnabled) expressServer.get('*', (req, res) => {
             return nextJsRequestHandler(req, res);
